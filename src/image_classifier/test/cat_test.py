ec0524cd862ac50d58b6a365e0613dde3c88ce94
Chris Vouga <crvouga@gmail.com>
2025-04-03 00:18:09 -0700

    Remove unused image files and update cat_test.py to reflect new image paths; introduce a helper function for asserting cat classification results.

--- src/image_classifier/test/cat_test.py
+++ src/image_classifier/test/cat_test.py
@@ -3,13 +3,7 @@ from src.image_classifier.classification import Classification
 from src.image_classifier.test.fixture import Fixture
 
 
-def test_cat_clear_front() -> None:
-    f = Fixture()
-
-    images = [Image.from_file("./images/cat_clear_front.jpeg")]
-
-    results = f.image_classifier.classify(images=images)
-
+def _assert_cat(results: list[Classification]) -> None:
     result = max(results, key=lambda x: x.weight)
 
     assert "cat" in result.label
@@ -18,10 +12,28 @@ def test_cat_clear_front() -> None:
 def test_cat_clear_front() -> None:
     f = Fixture()
 
-    images = [Image.from_file("./images/cat_security_footage.jpeg")]
+    images = [Image.from_file("./images/cat_clear_front/1.jpeg")]
 
     results = f.image_classifier.classify(images=images)
 
-    result = max(results, key=lambda x: x.weight)
+    _assert_cat(results)
 
-    assert "cat" in result.label
+
+def test_cat_clear_front_2() -> None:
+    f = Fixture()
+
+    images = [Image.from_file("./images/cat_clear_front/2.jpeg")]
+
+    results = f.image_classifier.classify(images=images)
+
+    _assert_cat(results)
+
+
+def test_cat_security_footage() -> None:
+    f = Fixture()
+
+    images = [Image.from_file("./images/cat_security_footage/1.jpeg")]
+
+    results = f.image_classifier.classify(images=images)
+
+    _assert_cat(results)
