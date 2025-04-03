78494eb969c57cff16b6201f5f76eb2ebf98d4ec
Chris Vouga <crvouga@gmail.com>
2025-04-02 23:18:16 -0700

    Enhance Makefile by adding type checking command and simplifying test command; update import path in inteface.py for consistency.

--- src/device_door/inteface.py
+++ src/device_door/inteface.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Literal
-from library.pub_sub import Sub
+from src.library.pub_sub import Sub
 
 
 @dataclass
