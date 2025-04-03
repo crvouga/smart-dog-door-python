bece4cb7f05717b02c80c7720d9986d448303ba0
Chris Vouga <crvouga@gmail.com>
2025-04-03 00:27:17 -0700

    Refactor image classification methods to use iterators instead of lists for improved memory efficiency; update interface and implementations in impl_fake.py and impl_yolo.py accordingly. Adjust tests in cat_test.py to accommodate the new iterator return type.

--- src/image_classifier/interface.py
+++ src/image_classifier/interface.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 from src.image.image import Image
 from src.image_classifier.classification import Classification
+from typing import Iterator
 
 
 class ImageClassifier(ABC):
     @abstractmethod
-    def classify(self, images: list[Image]) -> list[Classification]:
+    def classify(self, images: list[Image]) -> Iterator[Classification]:
         pass
