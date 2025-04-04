35002fbb64d8dcaced20f2e347f6267f017250a3
Chris Vouga <crvouga@gmail.com>
2025-04-04 16:45:35 -0700

    Refactor PubSub class to improve message handling with enhanced enqueue and map methods. Update VSCode settings to include unsub package for better development experience.

---
+++ src/image_classifier/test/dog_test.py
@@ -0,0 +1,61 @@
+from src.assets import assets_dir
+from src.image.image import Image
+from src.image_classifier.classification import Classification
+from src.image_classifier.test.fixture import Fixture
+
+
+def _test_dog(image_asset: str) -> None:
+    f = Fixture()
+
+    image = Image.from_file(assets_dir(image_asset))
+
+    results = f.image_classifier.classify(images=[image])
+
+    assert len(results) > 0
+
+    result = max(results, key=lambda x: x.weight)
+
+    assert "dog" in result.label
+
+
+def _test_not_dog(image_asset: str) -> None:
+    f = Fixture()
+
+    image = Image.from_file(assets_dir(image_asset))
+
+    results = f.image_classifier.classify(images=[image])
+
+    if len(results) == 0:
+        return
+
+    result = max(results, key=lambda x: x.weight)
+
+    assert "dog" not in result.label
+
+
+def test_dog_clear_front() -> None:
+    _test_dog("images/dog_clear_front/1.jpeg")
+
+
+def test_dog_clear_front_2() -> None:
+    _test_dog("images/dog_clear_front/2.jpeg")
+
+
+def test_dog_security_footage_1() -> None:
+    _test_dog("images/dog_security_footage/1.jpeg")
+
+
+def test_dog_security_footage_2() -> None:
+    _test_dog("images/dog_security_footage/2.jpeg")
+
+
+def test_dog_security_footage_3() -> None:
+    _test_dog("images/dog_security_footage/3.jpeg")
+
+
+def test_not_dog_dog_clear_front() -> None:
+    _test_not_dog("images/cat_clear_front/1.jpeg")
+
+
+def test_not_dog_person_clear_front() -> None:
+    _test_not_dog("images/person_clear_front/1.jpeg")
