601c8b28b7a7933f2558a464e0173925b1ab877a
Chris Vouga <crvouga@gmail.com>
2025-04-06 15:24:17 -0700

    Enhance door transition logic and add new state handling
    - Introduced functions to manage transitions to WillClose and Closed states, improving the door's state management.
    - Refactored existing transition functions for clarity and consistency in handling door states.
    - Updated tests to reflect changes in transition logic, ensuring accurate state transitions and robust testing.

--- src/smart_door/core/test/ready_door_close_test.py
+++ src/smart_door/core/test/ready_door_close_test.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from dataclasses import replace
 from src.image_classifier.classification import Classification
-from src.smart_door.core.model import DoorState, Model, ModelReady
+from src.smart_door.core.model import DoorState, ModelReady
 from src.smart_door.core.msg import MsgTick
 from src.smart_door.core.test.fixture import BaseFixture
 
@@ -14,7 +14,7 @@ class Fixture(BaseFixture):
 
         classifications = [
             Classification(
-                label="dog",
+                label="cat",
                 weight=0.5,
             ),
         ]
@@ -26,41 +26,49 @@ class Fixture(BaseFixture):
         )
 
 
-def test_transition_to_will_open_state_when_classifications_contain_unlock_list_objects() -> (
-    None
-):
-    f = Fixture()
-    model, _ = f.transition(model=f.model, msg=MsgTick(happened_at=datetime.now()))
-
-    assert isinstance(model, ModelReady)
-    assert model.door.state == DoorState.WillOpen
-
-
-def test_do_not_transition_to_will_open_state_if_door_is_already_open() -> None:
+def test_transition_to_will_close_state_when_open() -> None:
     f = Fixture(door_state=DoorState.Open)
     model, _ = f.transition(model=f.model, msg=MsgTick(happened_at=datetime.now()))
 
     assert isinstance(model, ModelReady)
-    assert model.door.state == DoorState.Open
+    assert model.door.state == DoorState.WillClose
 
 
-def test_transition_to_open_state_if_state_is_will_close() -> None:
-    f = Fixture(door_state=DoorState.WillClose)
+def test_stays_in_closed_state() -> None:
+    f = Fixture(door_state=DoorState.Closed)
     model, _ = f.transition(model=f.model, msg=MsgTick(happened_at=datetime.now()))
 
     assert isinstance(model, ModelReady)
-    assert model.door.state == DoorState.Open
+    assert model.door.state == DoorState.Closed
 
 
-def test_transition_to_open_state_after_minimal_duration_will_open() -> None:
+def test_transition_to_closed_state_if_door_state_is_will_open() -> None:
     f = Fixture(door_state=DoorState.WillOpen)
+    model, _ = f.transition(model=f.model, msg=MsgTick(happened_at=datetime.now()))
+
+    assert isinstance(model, ModelReady)
+    assert model.door.state == DoorState.Closed
+
+
+def test_transition_to_will_open_state_if_door_state_is_open() -> None:
+    f = Fixture(door_state=DoorState.WillOpen)
+    model, _ = f.transition(model=f.model, msg=MsgTick(happened_at=datetime.now()))
+
+    assert isinstance(model, ModelReady)
+    assert model.door.state == DoorState.Closed
+
+
+def test_transition_to_will_close_state_to_closed_state_after_minimal_duration_will_close() -> (
+    None
+):
+    f = Fixture(door_state=DoorState.WillClose)
 
     model, _ = f.transition(
         model=f.model,
         msg=MsgTick(
-            happened_at=datetime.now() + f.model.config.minimal_duration_will_open
+            happened_at=datetime.now() + f.model.config.minimal_duration_will_close
         ),
     )
 
     assert isinstance(model, ModelReady)
-    assert model.door.state == DoorState.Open
+    assert model.door.state == DoorState.Closed
--- src/smart_door/core/transition_ready_door.py
+++ src/smart_door/core/transition_ready_door.py
@@ -20,13 +20,81 @@ def transition_ready_door(
 ) -> tuple[ModelDoor, list[Effect]]:
     effects_new: list[Effect] = []
 
-    door, effects = _transition_door_will_open(model=model, door=door, msg=msg)
+    door, effects = _transition_door_to_will_open(model=model, door=door, msg=msg)
     effects_new.extend(effects)
 
     door, effects = _transition_from_will_open_to_open(model=model, door=door, msg=msg)
     effects_new.extend(effects)
 
+    door, effects = _transition_door_to_will_close(model=model, door=door, msg=msg)
+    effects_new.extend(effects)
+
+    door, effects = _transition_from_will_close_to_closed(
+        model=model, door=door, msg=msg
+    )
+    effects_new.extend(effects)
+
     return door, effects_new
+
+
+def _transition_door_to_will_close(
+    model: ModelReady, door: ModelDoor, msg: Msg
+) -> tuple[ModelDoor, list[Effect]]:
+    should_close = _does_have_close_list_objects(
+        classifications=model.camera.latest_classification,
+        close_list=model.config.classification_close_list,
+    )
+
+    if not should_close:
+        return door, []
+
+    if door.state == DoorState.Closed:
+        return door, []
+
+    if door.state == DoorState.WillOpen:
+        return replace(door, state=DoorState.Closed), []
+
+    return replace(door, state=DoorState.WillClose), []
+
+
+def _transition_from_will_close_to_closed(
+    model: ModelReady, door: ModelDoor, msg: Msg
+) -> tuple[ModelDoor, list[Effect]]:
+    if door.state != DoorState.WillClose:
+        return door, []
+
+    if not isinstance(msg, MsgTick):
+        return door, []
+
+    elapsed_time = msg.happened_at - door.state_start_time
+    if elapsed_time < model.config.minimal_duration_will_close:
+        return door, []
+
+    return replace(door, state=DoorState.Closed), []
+
+
+def _does_have_close_list_objects(
+    classifications: list[Classification],
+    close_list: list[ClassificationConfig],
+) -> bool:
+    return any(
+        _does_have_close_list_object(
+            classifications=classifications,
+            close_object=close_object,
+        )
+        for close_object in close_list
+    )
+
+
+def _does_have_close_list_object(
+    classifications: list[Classification],
+    close_object: ClassificationConfig,
+) -> bool:
+    return any(
+        classification.label.lower().strip() == close_object.label.lower().strip()
+        and classification.weight >= close_object.min_weight
+        for classification in classifications
+    )
 
 
 def _transition_from_will_open_to_open(
@@ -45,7 +113,7 @@ def _transition_from_will_open_to_open(
     return replace(door, state=DoorState.Open), []
 
 
-def _transition_door_will_open(
+def _transition_door_to_will_open(
     model: ModelReady, door: ModelDoor, msg: Msg
 ) -> tuple[ModelDoor, list[Effect]]:
     should_open = _does_have_open_list_objects(
@@ -53,11 +121,20 @@ def _transition_door_will_open(
         open_list=model.config.classification_open_list,
     )
 
-    if should_open and door.state != DoorState.Open:
-        door_next_state = (
-            DoorState.Open if door.state == DoorState.WillClose else DoorState.WillOpen
-        )
-        return replace(door, state=door_next_state), []
+    if not should_open:
+        return door, []
+
+    if door.state == DoorState.Open:
+        return door, []
+
+    if door.state == DoorState.WillClose:
+        return replace(door, state=DoorState.Open), []
+
+    if door.state == DoorState.Closed:
+        return replace(door, state=DoorState.WillOpen), []
+
+    if door.state == DoorState.WillOpen:
+        return replace(door, state=DoorState.WillOpen), []
 
     return door, []
 
@@ -67,10 +144,20 @@ def _does_have_open_list_objects(
     open_list: list[ClassificationConfig],
 ) -> bool:
     return any(
-        any(
-            classification.label.lower().strip() == config.label.lower().strip()
-            and classification.weight >= config.min_weight
-            for classification in classifications
+        _does_have_open_list_object(
+            classifications=classifications,
+            open_object=open_object,
         )
-        for config in open_list
+        for open_object in open_list
+    )
+
+
+def _does_have_open_list_object(
+    classifications: list[Classification],
+    open_object: ClassificationConfig,
+) -> bool:
+    return any(
+        classification.label.lower().strip() == open_object.label.lower().strip()
+        and classification.weight >= open_object.min_weight
+        for classification in classifications
     )
